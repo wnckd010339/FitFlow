dce52d7454b633a0cfafb48890fb38df64cd7bf6
wnckd010339 <wnckd010339@gmail.com>
2026-08-05 23:42:36 +0900

    회원관리의 상세보기 수정 및 결제보기 페이지 하드코딩

--- src/main/java/com/acorn/gymmanagement/membership/controller/MembershipController.java
+++ src/main/java/com/acorn/gymmanagement/membership/controller/MembershipController.java
@@ -1,6 +1,15 @@
 package com.acorn.gymmanagement.membership.controller;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
 @RequestMapping("/admin/memberships")
-public class MembershipController { }
+public class MembershipController {
+
+    @GetMapping
+    public String index() {
+        return "admin/membership/index";
+    }
+}
